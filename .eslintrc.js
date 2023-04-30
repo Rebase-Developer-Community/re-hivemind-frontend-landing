950834f720f3049e212434a11fb83f50168ad5c9
Pratima <amitrana@MacUser.local>
2023-04-30 20:30:36 +0530

    Changes for eslint files

---
+++ .eslintrc.js
@@ -0,0 +1,62 @@
+module.exports = {
+  env: {
+    amd: true,
+    browser: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@next/next/recommended',
+    'prettier',
+    'plugin:prettier/recommended', // Make sure this is always the last element in the array.
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  plugins: ['simple-import-sort', 'sort-destructure-keys', 'prettier'],
+  root: true,
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'jsx-a11y/anchor-is-valid': [
+      'error',
+      {
+        aspects: ['invalidHref', 'preferButton'],
+        components: ['Link'],
+        specialLink: ['hrefLeft', 'hrefRight'],
+      },
+    ],
+    'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+    'react/jsx-sort-props': [
+      'error',
+      {
+        callbacksLast: true,
+        ignoreCase: false,
+        noSortAlphabetically: false,
+        reservedFirst: true,
+        shorthandFirst: true,
+        shorthandLast: false,
+      },
+    ],
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'simple-import-sort/exports': 'error',
+    'simple-import-sort/imports': 'error',
+    'sort-destructure-keys/sort-destructure-keys': 2,
+    'sort-keys': ['error', 'asc', { caseSensitive: true, minKeys: 2, natural: false }],
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};

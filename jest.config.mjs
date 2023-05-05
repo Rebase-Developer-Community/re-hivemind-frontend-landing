8a58718bd97ad4624f699bea20092aa10e428f3d
Rishabh Jain <hello@rishabhjain.dev>
2023-05-06 00:51:46 +0530

    feat: added jest coverage action

--- jest.config.mjs
+++ jest.config.mjs
@@ -20,7 +20,15 @@ const config = {
     '!**/jest.setup.js',
     '!**/pages/_app.tsx',
     '!**/pages/_document.tsx',
-  ]
+  ],
+  coverageThreshold: {
+    global: {
+      statements: 80,
+      branches: 80,
+      functions: 80,
+      lines: 80,
+    },
+  }
 };
 
 export default createJestConfig(config);
